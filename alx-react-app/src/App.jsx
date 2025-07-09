658e531dcd5e643ce6ef3470e625d5e18e9c732b
Ojo Stephen <Ojostephen018@gmail.com>
2025-07-09 21:08:26 +0100

    made changes to file name

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
+
 
 function App() {
   const [count, setCount] = useState(0)
