85ac6491c751b6b08ed97ce23cf3a64e80942089
Ojo Stephen <Ojostephen018@gmail.com>
2025-07-10 12:40:42 +0100

    Fix: added WelcomeMessage.jsx in correct path with correct structure

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,14 @@
-import WelcomeMessage from "./components/WelcomeMessage";
+import WelcomeMessage from "./WelcomeMessage";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -31,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
