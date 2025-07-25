7e36cdfe6796e61ff10dfefea91555815accbe87
Ojo Stephen <Ojostephen018@gmail.com>
2025-07-25 14:46:58 +0100

    Fixing export issues

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,9 @@
+import Counter from "./components/Counter";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
-import Counter from "./components/Counter";
 
 function App() {
   return (
