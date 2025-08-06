4a5bfe2e37cfdd18db4447b3cafcc7ca3d7a12e0
Ojo Stephen <Ojostephen018@gmail.com>
2025-08-06 22:24:52 +0100

    Fixed typo error with file name

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
